72c97bb2ce9f63aa90a51748e4d1c899d920d0da
llsdah <77381506+llsdah@users.noreply.github.com>
2024-02-26 20:16:32 +0900

    graphql endpointcheck

--- src/main/java/com/fastcampus/miniorderproject/controller/GraphQLController.java
+++ src/main/java/com/fastcampus/miniorderproject/controller/GraphQLController.java
@@ -5,6 +5,7 @@ import graphql.ExecutionInput;
 import graphql.GraphQL;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +19,19 @@ public class GraphQLController {
     private GraphQL graphQL;
 
     @PostMapping(value = "/graphql", consumes = "application/json", produces = "application/json")
-    public ExecutionResult graphql(@RequestBody String query) {
+    public ResponseEntity<Object> graphql(@RequestBody String query) {
         log.info("GraphQL Start");
         log.info("GraphQL : "+query);
         ObjectMapper objectMapper = new ObjectMapper();
         //String test = objectMapper.
         log.debug("이거다 이거 ");
         ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
-        return graphQL.execute(executionInput);
+        ExecutionResult executionResult = graphQL.execute(executionInput);
+        log.info("executionResult.getData() "+executionResult.getData());
+        log.info("executionResult.getErrors() "+executionResult.getErrors());
+        return ResponseEntity.ok(executionResult.getData());
+        //return graphQL.execute(executionInput);
     }
 }
+
+
